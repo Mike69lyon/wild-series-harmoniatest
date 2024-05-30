57bf8fe11ec2c6e71bafd4d48ab0e69c9bab6273
Mike69lyon <161336444+Mike69lyon@users.noreply.github.com>
2024-05-30 17:08:32 +0200

    harmonia5

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,3 +1,5 @@
+
+
 create table user (
   id int unsigned primary key auto_increment not null,
   email varchar(255) not null unique,
@@ -9,4 +11,23 @@ create table item (
   title varchar(255) not null,
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
+);
+
+
+
+create table category (
+  id int unsigned primary key auto_increment not null,
+  title varchar(255) not null
+);
+
+
+create table program (
+  id int unsigned primary key auto_increment not null,
+  title varchar(255) not null,
+  synopsis text not null,
+  poster varchar(255) not null,
+  country varchar(255) not null,
+  year int not null,
+  category_id int unsigned not null,
+  foreign key(category_id) references category(id)
 );
